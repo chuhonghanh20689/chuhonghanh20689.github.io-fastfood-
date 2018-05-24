d82d639d95102cb9dde11dd683d3ad1d46854509
chuhonghanh20689 <30991836+chuhonghanh20689@users.noreply.github.com>
2018-05-24 23:01:25 +0700

    finish

---
+++ assets/js/common.js
@@ -0,0 +1,6 @@
+// const serverImagesUrl = "http://localhost:3000/product-assets/images/";
+// const serverThumbnailURL = "http://localhost:3000/product-assets/thumbnail/";
+var serverImagesUrl = "https://fries-salad.herokuapp.com/product-assets/images/";
+var serverThumbnailURL = "https://fries-salad.herokuapp.com/product-assets/thumbnail/";
+var adminEmail = "fries-salad.cs@gmail.com";
+var adminName = "Admin";
